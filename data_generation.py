e5a74c618e7fd969b29bf744ddd365ffd7e7f9a1
Roberto <rbattisti@fbk.eu>
2024-02-12 11:56:11 +0100

    Added comments and documentation for data_generation.py script.

--- data_generation.py
+++ data_generation.py
@@ -1,6 +1,7 @@
 """
 Script per preparare i dati per il training.
-Prende in input un GeoTIFF e uno Shapefile (poligoni dei pannelli) e genera tile RGB con relativa maschera binaria dei vari pannelli.
+Prende in input un GeoTIFF e lo Shapefile dei poligoni dei pannelli e genera tile RGB con relativa maschera binaria dei vari pannelli.
+E' possibile specificare la dimensione delle tiles (windows size) //#TDOO - e se scalare o meno l'immagine (resolution factor)//
 """
 
 import fiona
@@ -13,7 +14,7 @@ from shapely.strtree import STRtree
 import os
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description="Script to generate the data for training.")
+parser = ArgumentParser(description="Script to generate the data (images and masks) for training.")
 parser.add_argument('-g', '--geotiff', help='Path of the GeoTIFF file', required=True)
 parser.add_argument('-s', '--shapefile', help='Path of the Shapefile', required=True)
 parser.add_argument('-o', '--output', help='Output path (folder)', required=True)
@@ -30,128 +31,78 @@ window_size = int(args.window_size)
 with rasterio.open(geotiff_path) as src:
 
     polygons = []
-    
+
+    # Load the Shapefile and populate the list of PV polygons    
     with fiona.open(shapefile_path, "r") as pv_polygons_shapefile:
+
         for feature in pv_polygons_shapefile:
+
             geometry = shape(feature["geometry"])
-
-            #cy, cx = src.index(geometry.centroid.x, geometry.centroid.y)
-
             polygons.append(geometry)
             
+    # Create kd-tree with the polygons
     tree = STRtree([polygon for polygon in polygons])
 
     print(src.width)
     print(src.height)
-    print(src.index(701794.874, 4970877.671))
 
+    # Sliding window over the GeoTIFF, with 50% overlap between tiles
     for x in range(0, src.width, window_size // 2):
 
-        for y in range(0, src.height, window_size // 2): 
+        for y in range(0, src.height, window_size // 2):
 
-            window_w = window_size 
+            window_w = window_size
             window_h = window_size
                 
+            # Leave out the last tile
+            # TODO - get the tile at width - window_w
             if x + window_size > src.width:
                 continue
 
+            # Leave out the last tile
+            # TODO - get the tile at width - window_h
             if y + window_size > src.height:
                 continue
 
             print(x, y, "-", window_w, window_h)
             
+            # Windowed reading of RGB values from the GeoTiFF
+            # https://rasterio.readthedocs.io/en/stable/topics/windowed-rw.html
             r = src.read(1, window=Window(x, y, window_w, window_h)).reshape((window_w * window_h, 1))
             g = src.read(2, window=Window(x, y, window_w, window_h)).reshape((window_w * window_h, 1))
             b = src.read(3, window=Window(x, y, window_w, window_h)).reshape((window_w * window_h, 1))
 
+            # Create RGB image and empty mask
             rgb = np.stack((b, g, r), axis = 1).reshape(window_h, window_w, 3)
-            mask = np.zeros((window_h, window_w, 1), dtype = np.uint8)            
+            mask = np.zeros((window_h, window_w, 1), dtype = np.uint8)
 
-            found = False            
+            found = False
 
+            # Create a Polygon with the world coordinates of the tile
             tile = Polygon([src.xy(y, x), src.xy(y + window_h, x), src.xy(y + window_h, x + window_w), src.xy(y, x + window_w)])
             
-            result = tree.query(tile)                            
+            # Search all the PV Polygons that intersect the tile using the kd-tree
+            result = tree.query(tile)
                         
             for index in result:
-
-                polygon = polygons[index]                
+                
+                polygon = polygons[index]
                 dilated = polygon.buffer(0.1)
                 points = []
 
+                # Convert PV Polygon coordinates to image space
+                # https://rasterio.readthedocs.io/en/stable/quickstart.html#spatial-indexing
                 for xi, yi in dilated.exterior.coords:
                     py, px = src.index(xi, yi)
 
                     points.append([px - x, py - y])
                     
+                # Fill the polygon on the mask image
                 cv2.fillPoly(mask, pts=[np.array(points)], color=(255))
                     
                 found = True
             
-            if found and np.count_nonzero(mask) / (window_w * window_h) > 0.1:                
+            # Write output image and mask only if the PV covers a certain amount of the image
+            if found and np.count_nonzero(mask) / (window_w * window_h) > 0.1:
                 cv2.imwrite(os.path.join(output_path, "images", f"shp_to_mask_{x}_{y}.png"), rgb)
                 cv2.imwrite(os.path.join(output_path, "masks", f"shp_to_mask_{x}_{y}.png"), mask)
-                
-# with rasterio.open(geotiff_path) as src:
-    
-#     polygons = []
-
-#     with fiona.open(shapefile_path, "r") as pv_polygons_shapefile:
-#         for feature in pv_polygons_shapefile:
-#             geometry = shape(feature["geometry"])
-
-#             cy, cx = src.index(geometry.centroid.x, geometry.centroid.y)
-
-#             polygons.append((geometry, (cx, cy)))
-            
-#     print(src.width)
-#     print(src.height)
-
-    # for x in range(0, src.width, window_size):
-
-    #     for y in range(0, src.height, window_size): 
-
-    #         window_w = window_size 
-    #         window_h = window_size
-                
-    #         if x + window_size > src.width:
-    #             continue
-
-    #         if y + window_size > src.height:
-    #             continue
-
-    #         print(x, y, "-", window_w, window_h)
-            
-    #         r = src.read(1, window=Window(x, y, window_w, window_h)).reshape((window_w * window_h, 1))
-    #         g = src.read(2, window=Window(x, y, window_w, window_h)).reshape((window_w * window_h, 1))
-    #         b = src.read(3, window=Window(x, y, window_w, window_h)).reshape((window_w * window_h, 1))
-
-    #         rgb = np.stack((b, g, r), axis = 1).reshape(window_h, window_w, 3)
-    #         mask = np.zeros((window_h, window_w, 1), dtype = np.uint8)            
-
-    #         found = False            
-
-    #         tile = Polygon([src.xy(x, y), src.xy(x + window_w, y), src.xy(x + window_w, y + window_h), src.xy(x, y + window_h)])
-
-    #         for polygon in polygons:
-                
-    #             #center = polygon[1]                
-
-    #             #if center[0] >= x and center[0] <= x + window_w and center[1] >= y and center[1] <= y + window_h:
-    #             if polygon[0].intersects(tile):
-                    
-    #                 points = []
-    #                 dilated = polygon[0].buffer(0.1)
-    #                 for xi, yi in dilated.exterior.coords:
-    #                     py, px = src.index(xi, yi)
-
-    #                     points.append([px - x, py - y])
-                        
-    #                 cv2.fillPoly(mask, pts=[np.array(points)], color=(255))
-                    
-    #                 found = True
-            
-    #         if found:
-    #             cv2.imwrite(os.path.join(output_path, "images", f"shp_to_mask_{x}_{y}.png"), rgb)
-    #             cv2.imwrite(os.path.join(output_path, "masks", f"shp_to_mask_{x}_{y}.png"), mask)
-    #             exit()
